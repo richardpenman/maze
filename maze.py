f0dbd1347faa9b184e2d57fca4842f2e71370b3c
Richard Penman <richard.penman@gmail.com>
2022-01-10 16:46:11 -0500

    added video output

--- maze.py
+++ maze.py
@@ -23,11 +23,16 @@ def get_neighbors(p, width, height):
 
 
 def render_maze(removed_edges, width, height):
-    """Render maze to an image
+    """Generate an iterator of frames to build a maze
     """
     image = Image.new('1', (2 * width + 1, 2 * height + 1))
     pixels = image.load()
-    # add edges
+
+    # add entrance
+    pixels[1, 0] = 1
+    yield image
+
+    # add nodes and edges
     for a, b in removed_edges:
         a_x, a_y = a
         b_x, b_y = b
@@ -41,18 +46,38 @@ def render_maze(removed_edges, width, height):
         elif a_y > b_y:
             wall_y -= 1
         pixels[wall_x + 1, wall_y + 1] = 1
-    # add points
-    for y in range(height):
-        for x in range(width):
-            pixels[x * 2 + 1, y * 2 + 1] = 1
-    # add entrances
-    pixels[1, 0] = 1
+        pixels[a_x * 2 + 1, a_y * 2 + 1] = 1
+        pixels[b_x * 2 + 1, b_y * 2 + 1] = 1
+        yield image
+
+    # add exit
     pixels[2 * width - 1, 2 * height] = 1
+    yield image
+
+
+def save_image(filename, frames, scale):
+    """Save the final frame as an image
+    """
+    image = None
+    # get the final frame
+    for image in frames:
+        pass
     # upscale the image
-    return image
+    width, height = image.size
+    image.resize((width * scale, height * scale)).save(filename)
 
 
-def create_maze(width, height):
+def save_video(filename, frames, scale, duration):
+    """Save frames as a video
+    """
+    images = []
+    for image in frames:
+        width, height = image.size
+        images.append(image.resize((width * scale, height * scale)).convert('L'))
+    images[0].save(fp=filename, format='GIF', append_images=images[1:], save_all=True, duration=duration, loop=0)
+
+
+def build_maze(width, height):
     """Create a maze of these dimensions and return the edges that have been removed
     """
     start = 0, 0
@@ -77,13 +102,20 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--width', type=int, default=50, help='The width of the maze (default 50)')
     parser.add_argument('--height', type=int, default=50, help='The height of the maze (default 50)')
-    parser.add_argument('--filename', default='maze.png', help='The image filename where to save the result (default maze.png)')
-    parser.add_argument('--scale', type=int, default=100, help='The scale of the rendered image (default 100)')
+    parser.add_argument('--filename', help='The filename where to save the result')
+    parser.add_argument('--video', action='store_true', help='Whether to generate video output instead of image')
+    parser.add_argument('--scale', type=int, default=100, help='The scale of the rendered output (default 100)')
+    parser.add_argument('--duration', type=int, default=200, help='The duration of the rendered video (default 200)')
     args = parser.parse_args()
 
-    removed_edges = create_maze(args.width, args.height)
-    image = render_maze(removed_edges, args.width, args.height)
-    image.resize((args.width * args.scale, args.height * args.scale)).save(args.filename)
+    removed_edges = build_maze(args.width, args.height)
+    frames = render_maze(removed_edges, args.width, args.height)
+    if args.video:
+        # generate video output
+        save_video(args.filename or 'maze.gif', frames, args.scale, args.duration)
+    else:
+        # generate image output
+        save_image(args.filename or 'maze.png', frames, args.scale)
 
 
 if __name__ == '__main__':
